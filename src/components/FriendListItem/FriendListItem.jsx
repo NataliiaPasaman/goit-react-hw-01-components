cb4d4b5041844ee6ded4af20b50a06acb021f3f0
NataliiaPasaman <natapasaman@gmail.com>
2022-10-26 00:10:06 +0300

    add FriendList css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,19 +1,14 @@
 import css from 'components/FriendListItem/FriendListItem.module.css';
 import PropsTypes from 'prop-types';
-/**
- * Опис компонента <FriendListItem>
-Компонент повинен приймати кілька пропів:
-avatar - посилання на аватар
-name - ім'я друга
-isOnline - буль, що сигналізує про стан друга: в мережі або ні.
-Залежно від пропа isOnline, повинен змінюватися колір фону span.status. 
-Це можна зробити за допомогою різних CSS-класів або Styled Components.
- */
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}>{isOnline}</span>
+      {isOnline ? (
+        <span className={css.isOnline}>{isOnline}</span>
+      ) : (
+        <span className={css.status}>{isOnline}</span>
+      )}
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
