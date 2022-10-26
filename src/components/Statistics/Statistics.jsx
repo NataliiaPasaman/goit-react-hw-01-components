7059d1e9fe96e891418bf71fce4955ca74a1ee1c
NataliiaPasaman <natapasaman@gmail.com>
2022-10-26 19:04:00 +0300

    fixed title in Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -13,7 +13,7 @@ const getBgColor = () => {
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title.length > 0 ? <h2 className={css.title}>{title}</h2> : null}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat_list}>
         {stats.map(({ id, label, percentage }) => {
           return (
